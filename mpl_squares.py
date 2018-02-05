b9dc434da4c446800f49d43d205f60638fbea9ff
zhipc <zhipeter166@gmail.com>
2018-02-05 16:01:42 +0800

    Start: draw simple picture

---
+++ mpl_squares.py
@@ -0,0 +1,18 @@
+'''
+use matplotlib to draw
+'''
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth=5)
+
+# set picture caption and tag on axis
+plt.title("Squares Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# set scale
+plt.tick_params(axis='both', labelsize=14)
+
+plt.show()
